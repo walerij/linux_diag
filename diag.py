8142e5789be2732f047fd5ed334a84a5dfad4c30
Zhdanov Valery <becwal@yandex.ru>
2022-06-24 18:48:36 +0300

    Информация об операционной системе

--- diag.py
+++ diag.py
@@ -1,7 +1,9 @@
 #информация об операционной системе
 import os
+import platform
 print("Система:"+os.name)
+print("Операционная система:")
+print (platform.uname())
 print("Логин:"+os.getlogin() )
 print("Количество CPU:"+str(os.cpu_count()))
-print(os.get_terminal_size())
-print(os.getpid())
+
